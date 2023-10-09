f882c783fd39edfe52357e247b5fb8b34a5e3b10
RafaZisol <124323241+RafaZisol@users.noreply.github.com>
2023-10-08 18:35:49 -0600

    Quedó Codigo 1

---
+++ Codigo1/js/anticesar.js
@@ -0,0 +1,27 @@
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textodescifrado = document.getElementById("cifrado")
+console.log(desplazamiento.value);
+//Creamos funcion para cifrar
+function cifrado(){
+    //Declarar el texto a ingresar
+    const textoIngresado = texto.value;
+    textodescifrado.value = textoIngresado.split('').map(c=>{
+        let mayus = (c===c.toUpperCase()) ? true :
+        false;
+        let valorEntero = c.toLowerCase().charCodeAt(0);
+        if(valorEntero >= 97 && valorEntero <= 122){
+            const valorDesplazamiento = parseInt(desplazamiento.value);
+            console.log(desplazamiento.value);
+            if(valorEntero-valorDesplazamiento < 97){                
+                valorEntero=123 - (97 - (valorEntero-valorDesplazamiento));
+            }else{
+                valorEntero=valorEntero-valorDesplazamiento
+            }
+        }
+        let cifrado = String.fromCharCode(valorEntero);
+        return mayus ? cifrado.toUpperCase() : cifrado;
+    }).join('');
+}
+texto.addEventListener("keyup",cifrado);
+desplazamiento.addEventListener("change", cifrado);
