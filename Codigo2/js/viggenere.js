0a48b48220160af17a04a74af6742969b4edc1b1
RafaZisol <rafgg2609@gmail.com>
2023-10-10 18:51:06 -0600

    viggenere

---
+++ Codigo2/js/viggenere.js
@@ -0,0 +1,29 @@
+var viggenere = viggenere || (function(){
+    var doStaff = function(txt, desp, action){
+        var replace = (function(){
+            var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            var l = abc.length;
+            return function(c){
+                var i = abc.indexOf(c.toLowerCase())
+                if (i != -1){
+                    var pos = i;
+                    if(action){
+                        //Cifrar
+                        pos += desp;
+                        pos = (pos>=1)?pos-1 : pos;
+                    }else{
+                        //Descifar
+                        pos -= desp;
+                        pos = (pos<0)?l+pos : pos;
+                    }
+                    return abc[pos];
+                }
+                return c;
+            }
+        })();
+        var re = (/([a-z])/ig);
+        return String(txt).replace(re,function(match){
+            return replace(match);
+        })
+    }
+});
