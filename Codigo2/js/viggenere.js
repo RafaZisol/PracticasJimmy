d2697fe6c64a6aaa969a410d2de1d5693d4aa502
RafaZisol <rafgg2609@gmail.com>
2023-10-17 18:29:15 -0600

    actu

--- Codigo2/js/viggenere.js
+++ Codigo2/js/viggenere.js
@@ -26,4 +26,88 @@ var viggenere = viggenere || (function(){
             return replace(match);
         })
     }
-});
+    return{
+        encode:function(txt,desp){
+            return doStaff(txt,desp,true);
+        },
+        decode : function(txt,desp){
+            return doStaff(txt,desp,false);
+        }
+    };
+})();
+
+function longitudCifrar(){
+    camposVacios();
+    var texto = document.getElementById("txt").value;
+    var clave = docuemnt.getElementById("txtClave").value;
+    if(clave.length > texto.length){
+        alert("La clave no puede ser mas grande que el texto a cifrar");
+    }else{
+        codificar(texto,clave);
+    }
+}
+
+function longitudDescifrar(){
+    camposVacios();
+    var texto = document.getElementById("txt").value;
+    var clave = docuemnt.getElementById("txtClave").value;
+    if(clave.length > texto.length){
+        alert("La clave no puede ser mas grande que el texto a cifrar");
+    }else{
+        decodificar(texto,clave);
+    }
+}
+
+function codificar (texto,clave){
+    var resultado="";
+    var indiceClave = 0;
+    var charATexto = texto.split('');
+
+    for(var i=0; i<charATexto.length; i++){
+        var des = obIndiceClave(Clave.charAt(indiceClave));
+        var charTexto = charATexto[i];
+
+        resultado += viggenere.encode(charTexto, (des>=26)? des%26 : des);
+        indiceClave++;
+
+        if (indiceClave>=clave.length){
+            indiceClave =0
+        }
+    }
+    document.getElementById("res").value=resultado;
+}
+
+function decodificar (texto,clave){
+    var resultado="";
+    var indiceClave = 0;
+    var charATexto = texto.split('');
+
+    for(var i=0; i<charATexto.length; i++){
+        var des = obIndiceClave(Clave.charAt(indiceClave));
+        var charTexto = charATexto[i];
+
+        resultado += viggenere.encode(charTexto, (des>=26)? des%26 : des);
+        indiceClave++;
+
+        if (indiceClave>=clave.length){
+            indiceClave =0
+        }
+    }
+    document.getElementById("res").value=resultado;
+}
+
+function obIndiceClave(reco){
+    var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return abc.indexOf(reco.toLowerCase());
+}
+
+function camposVacios(){
+    var texto = document.getElementById("txt").value;
+    var clave = docuemnt.getElementById("txtClave").value;
+    if (texto==""){
+        alert="Ingrese un texto para cifrar"
+    }
+    if (clave==""){
+        alert="Ingrese una clave para cifrar"
+    }
+}
